d991bc30fc5819e67c54d357ae8b495c1e4c0258
Angelia <thompson27thompson@gmail.com>
2022-09-07 01:37:10 -0400

    Basic Operater Setup and HTML Skeleton

---
+++ main.js
@@ -0,0 +1,21 @@
+// Operator Functions
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
+
+function operate(operator, num1, num2) {
+    
+}
